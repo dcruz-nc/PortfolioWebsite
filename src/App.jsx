8249c04c85c34069d343098c2ae7f0bfeba9b2c6
David Cruz <dcruz.nc@gmail.com>
2025-08-25 19:58:03 -0400

    cursor white ball

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { Home } from "./components/sections/Home";
 import { About } from "./components/sections/About";
 import { Projects } from "./components/sections/Projects";
 import { Contact } from "./components/sections/Contact";
+import { CustomCursor } from "./components/CustomCursor";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -21,6 +22,7 @@ function App() {
           isLoaded ? "opacity-100" : "opacity-0"
         } bg-black text-gray-100`}
       >
+        <CustomCursor />
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <Home />
